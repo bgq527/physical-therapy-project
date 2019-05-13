922151113c746a42aca0a4ef8a39aa2d5438b22b
bgq527 <bgq527@mocs.utc.edu>
2019-05-13 09:09:11 -0400

    Added ColorPicker GUI (judah4/HSV-Color-Picker-Unity) Canvas on 1modelcomp scene

---
+++ PhysTherapy Proj/Assets/Scripts/MainGUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MainGUI : MonoBehaviour {
+
+	public Rect window = new Rect(15, 15, 250, 250);
+
+	private void OnGUI(){
+		window = GUI.Window(0, window, mainGUIWindow, "Options");
+	}
+
+	private void mainGUIWindow(int windowID){
+		GUI.Button(new Rect(15, 15, 50, 50), "Button");
+
+		GUI.DragWindow();
+	}
+}
---
+++ PhysTherapy Proj/Assets/Scripts/changeColor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Assets.HSVPicker;
+
+public class changeColor : MonoBehaviour {
+
+	public Renderer objRenderer;
+	public ColorPicker picker;
+	// Use this for initialization
+	void Start () {
+		objRenderer = gameObject.GetComponent<Renderer>();
+		picker.onValueChanged.AddListener(color =>
+		{
+			objRenderer.material.color = color;
+		});
+		objRenderer.material.color = picker.CurrentColor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
